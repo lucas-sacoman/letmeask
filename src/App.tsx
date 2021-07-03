12945948cc528484d0e064dbfa6667d998b2a992
Lucas Sacoman <lucas.sacoman1999@gmail.com>
2021-07-02 23:20:52 -0300

    added firebase

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
-          <Route path="/rooms/:id" component={Room} />
           <Route path="/admin/rooms/:id" component={AdminRoom} />
+          <Route path="/rooms/:id" component={Room} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter> 
